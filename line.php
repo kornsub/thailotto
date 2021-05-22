1fa6811bce85f9f2bb3de094682c754ff00ef36a
kornsub <kornsub.s@gmail.com>
2021-05-22 22:05:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,13 +1,1 @@
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My LIFF v2</title>
-  <style>
-    #pictureUrl { display: block; margin: 0 auto }
-  </style>
-</head>
-<body>
-  <h1>hi</h1>
-</body>
-</html>
+<?php echo "hi"; ?>
